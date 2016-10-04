fc8ab8ab9bbbab9740c9269da8d69fe72c977faf
Ricardo Dias <cadu.hd@gmail.com>
2016-10-04 19:03:54 -0300

    added enter functionality

---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+var html = document.getElementsByTagName('html')
+var boxRicardo = document.getElementById("js-square-ricardo")
+var boxRoberto = document.getElementById("js-square-roberto")
+
+ html[0].addEventListener("keydown", function(event) {
+    if (event.which == 37){ //left
+      boxRoberto.className = 'square three columns'
+      boxRicardo.className = 'square three columns is-active'
+    }else if (event.which == 39) { //rigth
+      boxRicardo.className = 'square three columns'
+      boxRoberto.className = 'square three columns is-active'
+    }
+    
+    if(event.which == 13){
+      var activeRicardo = document.getElementById("js-square-ricardo").classList.contains("is-active")
+      var activeRoberto = document.getElementById("js-square-roberto").classList.contains("is-active")
+
+      if(activeRicardo || activeRoberto)
+        window.location.href = "/" + (activeRicardo ? "ricardo" : "roberto")
+    }
+  })
