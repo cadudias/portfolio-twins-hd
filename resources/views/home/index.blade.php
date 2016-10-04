7dd84ac67d1c392085283e56a8e89c2e5c4086b8
Ricardo Dias <cadu.hd@gmail.com>
2016-10-03 23:55:21 -0300

    home styles adjustments

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,49 +2,72 @@
 
 @section('content')
 
-<div class="home container-fluid">
+<div class="home container container-wide">
 
     <div class="row">
         <h2 class="title">Choose Your Character</h2>
     </div>
 
-    <div class="row container">
+    <div class="row">
         <p class="text-about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat inventore possimus quisquam libero aspernatur deleniti explicabo, 
         magnam quaerat sed. Quae deleniti aperiam ut cumque reiciendis modi est, tempora ratione aliquid.</p>
     </div>
 
     <div class="row">
-        <div class="character four columns">
+        <div class="character five columns">
             <!--div class="photo"></div-->
             <img src="../images/ricardo.png" class="character-image u-max-full-width" alt="Ricardo H. Dias" width="500px">
-            <div class="name">Ricardo H. Dias</div>
+            <div class="name">Ricardo</div>
         </div>
 
-        <div class="selection-area four columns">
+        <div class="two columns">
+            &nbsp;
+        </div>
 
+        <div class="character five columns">
+            <!--div class="photo"></div-->
+            <img src="../images/roberto.png" class="u-max-full-width" alt="Roberto H. Dias">
+            <div class="name">Roberto</div>
+        </div>
+
+        <div class="selection-area">
             <div class="row">
 
                 <div class="square three columns">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
 
-                <div class="square three columns big">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                 <div class="square three columns">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
 
-                <div class="square three columns big">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                 <div class="square three columns">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
+
+               
 
                 <div class="square three columns">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
 
             </div>
             
             <div class="row">
-                <div class="infinity-symbol two columns">
-                    <img src="../images/infinity-symbol.png" class="u-max-full-width" alt="Simbolo de Infinito">
+                <div class="square three columns">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                </div>
+
+                 <div class="square three columns">
+                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                </div>
+
+                <div class="square three columns">
+                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                </div>
+
+                <div class="square three columns">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
             </div>
             
@@ -52,28 +75,26 @@
             <div class="row">
 
                 <div class="square three columns">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
 
                 <div class="square three columns">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
                 
                 <div class="square three columns">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
 
                 <div class="square three columns">
-                    <img src="../images/ricardo.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
+                    <img src="../images/question-mark.png" class="u-max-full-width" alt="Selecionar Ricardo H. Dias">
                 </div>
             </div>
 
-        </div>
+                            <!--div class="infinity-symbol six columns">
+                    <img src="../images/infinity-symbol.png" class="image u-max-full-width" alt="Simbolo de Infinito">
+                </div-->
 
-        <div class="character four columns">
-            <!--div class="photo"></div-->
-            <img src="../images/roberto.png" class="u-max-full-width" alt="Roberto H. Dias">
-            <div class="name">Roberto H. Dias</div>
         </div>
    </div>
 
