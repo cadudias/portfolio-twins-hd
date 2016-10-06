44f2295cc4c0e1c02923c2116746bcd89390fe3b
Ricardo Dias <cadu.hd@gmail.com>
2016-10-06 09:26:04 -0300

    used classList for adding and removing classes

--- public/js/home.js
+++ public/js/home.js
@@ -8,17 +8,17 @@ var characterRoberto = document.getElementById("js-character-roberto")
 
  html[0].addEventListener("keydown", function(event) {
     if (event.which == 37){ //left
-      boxRoberto.className = 'square three columns'
-      boxRicardo.className = 'square three columns is-active'
+      boxRoberto.classList.remove("is-active");
+      characterRoberto.classList.remove("is-active");
 
-      characterRoberto.className = 'character five columns'
-      characterRicardo.className = 'character five columns is-active'
+      boxRicardo.classList.add("is-active");
+      characterRicardo.classList.add("is-active")
     }else if (event.which == 39) { //rigth
-      boxRicardo.className = 'square three columns'
-      boxRoberto.className = 'square three columns is-active'
+      boxRicardo.classList.remove("is-active");
+      characterRicardo.classList.remove("is-active");
 
-      characterRicardo.className = 'character five columns'
-      characterRoberto.className = 'character five columns is-active'
+      boxRoberto.classList.add("is-active");
+      characterRoberto.classList.add("is-active")
     }
     
     if(event.which == 13){
